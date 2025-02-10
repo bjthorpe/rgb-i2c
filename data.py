d798c3617d4142b74f130161d0972cc825fd4afc
Ava Dean <ava.dean@york.ac.uk>
2025-02-10 15:44:59 +0000

    Turned DataPoint class into Event. Turned Information class into DataPoint.

--- data.py
+++ data.py
@@ -23,59 +23,53 @@ def process_data(file_=None,
 
     # TODO: if a file is provided, read it instead of using the test data.
 
-    # List of all the pieces of information for the simulation: (x, y, time, energy). TODO: remove after testing.
-    test_information = [(3, 3, 1.00, 18.0),
-                        (3, 3, 2.75, 20.0)]
+    # List of all the data points for the simulation: (time, side, x, y, energy). TODO: remove after testing.
+    test_data = [(1.00, 0, 3, 3, 18.0),
+                 (2.75, 0, 3, 3, 20.0)]
 
     assert color_method in COLOR_METHODS, f'{color_method} is an unknown colour method.'
 
     # Build up the data based on the provided colour method.
-    information = []  # Each piece of information which we have read in from file.
-    data = []  # The final pieces of data points after the information is processed below.
+    data_points = []  # Each data point which we have read in from file.
 
     if color_method == 'energy':
 
-        for x, y, t, e in test_information:  # x, y, time, energy. # TODO: don't use test_information.
-            num_ticks = Information.get_num_ticks_from_energy(e, energy_tick_rate)
-            alight_time = Information.get_alight_time(num_ticks, gradient_delay)
+        for t, s, x, y, e in test_data:  # time, side, x, y, energy. # TODO: don't use test_data.
+            num_ticks = DataPoint.get_num_ticks_from_energy(e, energy_tick_rate)
+            alight_time = DataPoint.get_alight_time(num_ticks, gradient_delay)
 
-            information.append(Information(x, y, e, num_ticks, start_time=t, end_time=t+alight_time))
+            data_points.append(DataPoint(x, y, e, num_ticks, start_time=t, end_time=t+alight_time))
 
-        information = sorted(information)  # Sorted based on start_time.
+        data_points = sorted(data_points)  # Sorted based on start_time.
 
         # We need to make sure that any hits on pixels that are already lit up do not overwrite, but instead add, energy to the pixel.
-        for n, iA in enumerate(information):  # i for piece of information.
+        for n, dA in enumerate(data_points):  # d for data point.
 
-            # Only bother look at pieces of information ahead of the currently considered one.
-            for iB in information[n+1:]:
+            # Only bother look at data points ahead of the currently considered one.
+            for dB in data_points[n+1:]:
 
-                # We're looking for pieces of information that hit the same pixel and iB starts before the end of iA.
-                if (iA.x == iB.x) and (iA.y == iB.y) and (iB.start_time < iA.end_time):
+                # We're looking for data points that hit the same pixel and iB starts before the end of iA.
+                if (dA.x == dB.x) and (dA.y == dB.y) and (dB.start_time < dA.end_time):
 
-                    # An event, iB, occurs within the time frame that iA is still alight.
+                    # An event, dB, occurs within the time frame that dA is still alight.
 
                     # Therefore, we erase the ticks of the initial iA event that would occur after iB has started.
                     # This includes remove the final background colour tick of iA, which iB will now deal with.
-                    iA.ticks -= Information.get_num_ticks_from_time(iA.end_time-iB.start_time, gradient_delay)
+                    dA.ticks -= DataPoint.get_num_ticks_from_time(dA.end_time-dB.start_time, gradient_delay)
 
                     # The initial iA event end time is now equal to the latter iB event start time.
-                    iA.end_time = iB.start_time
+                    dA.end_time = dB.start_time
 
                     # The energy of the latter iB event will be itself plus |the energy of the initial iA event minus the amount it has decayed by|.
-                    iB.energy += iA.energy - iA.ticks * energy_tick_rate
+                    dB.energy += dA.energy - dA.ticks * energy_tick_rate
 
                     # Now re-compute the (greater) number of ticks for the latter iB event.
-                    iB.ticks = Information.get_num_ticks_from_energy(iB.energy, energy_tick_rate)
+                    dB.ticks = DataPoint.get_num_ticks_from_energy(dB.energy, energy_tick_rate)
 
                     break
 
 
-        for i in information:  # i for piece of information.
-            # We can now generate all the data points of pixel lights-up via this piece of information.
-            data_points = i.get_data_points(color_gradient, energy_tick_rate, gradient_delay)
-
-            # We just now add them to our list of data points.
-            data += data_points
+        data = sum([d.get_events(color_gradient, energy_tick_rate, gradient_delay) for d in data_points], [])
 
     else:
         raise ValueError(f'{color_method} is an unknown colour method.')
@@ -83,7 +77,7 @@ def process_data(file_=None,
     return data
 
 
-class Information:
+class DataPoint:
     def __init__(self, x, y, energy, ticks, start_time, end_time):
         assert isinstance(x, int)
         assert isinstance(y, int)
@@ -105,18 +99,18 @@ class Information:
     def __repr__(self):
         return f'({self.x},{self.y})  {self.energy:6.2f}  {self.ticks}  {self.start_time:6.2f}  {self.end_time:6.2f}'
 
-    def get_data_points(self, color_gradient=COLOR_GRADIENT_DEFAULT,
+    def get_events(self, color_gradient=COLOR_GRADIENT_DEFAULT,
                         energy_tick_rate=ENERGY_TICK_RATE_DEFAULT, gradient_delay=GRADIENT_DELAY):
-        data_points = []
+        events = []
 
         for tick in range(self.ticks+1):
             energy = self.energy - tick * energy_tick_rate
 
             color = COLOR_DEFAULT if energy <= 0.0 else get_color_from_gradient(energy, color_gradient)
 
-            data_points.append(DataPoint(self.x, self.y, color, self.start_time+tick*gradient_delay))
+            events.append(Event(self.x, self.y, color, self.start_time+tick*gradient_delay))
 
-        return data_points
+        return events
 
     @staticmethod
     def get_num_ticks_from_energy(energy, energy_tick_rate=ENERGY_TICK_RATE_DEFAULT):
@@ -131,7 +125,7 @@ class Information:
         return num_ticks * gradient_delay
 
 
-class DataPoint:
+class Event:
     def __init__(self, x, y, color, start_time):
         assert isinstance(x, int)
         assert isinstance(y, int)
