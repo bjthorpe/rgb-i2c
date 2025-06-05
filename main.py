3007765be0e8dcb9e2ff00ce5ed192f0185e4862
Ava Dean <ava.dean@york.ac.uk>
2025-06-05 17:40:35 +0100

    Updated main.py to more sensible values.

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from manager import run
 from smbus import SMBus
 
 
-# Temporary data file for testing.
-temp_file = 'test_data/4.dat'  # Data file.
-layout = (4, 3)
+file_ = 'test_data/4.dat'  # Data file.
+layout = (4, 4)
 force_displays = False  # If we request more displays than addresses found, should we reuse displays anyway?
 normalise = True  # Should we re-normalise the times of the data to have on avg. 100 data points per 5 sec?
 
 bus = SMBus(1)
 displays = get_displays(bus, layout=layout)
 
+# Displays need to be organised.
+# Use...
+#print(display_arranger(bus, displays))
+# ... to see a graphic on how to arrange the displays.
 switch_displays_from_chars(displays, 'F', 'C')
 switch_displays_from_chars(displays, 'D', 'E')
 switch_displays_from_chars(displays, 'A', 'C')
@@ -19,11 +22,9 @@ switch_displays_from_chars(displays, 'B', 'E')
 switch_displays_from_chars(displays, 'B', 'C')
 switch_displays_from_chars(displays, 'E', 'G')
 
-#print(display_arranger(bus, displays));exit()
-
-run(file_=temp_file, layout=layout, \
+run(file_=file_, layout=layout, \
     bus=bus, displays=displays, \
-    mode='phase', \
-    energy_method='tick', \
+    mode='normal', \
+    energy_method='accumulate', \
     force_displays=force_displays, normalise=normalise)
 
